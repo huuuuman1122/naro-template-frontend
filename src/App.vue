a51e0dd4a084b965cf036aff9d24513dd8077c54
huuuuman1122 <kojimasola@icloud.com>
2026-06-12 17:55:04 +0900

    fix: added ClickCounter.vue reference to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import WelcomeMessage from './components/WelcomeMessage.vue'
+import ClickCounter from './components/ClickCounter.vue'
 </script>
 
 <template>
   <main>
     <WelcomeMessage msg="Webエンジニアになろう講習会へようこそ" />
+    <ClickCounter />
   </main>
 </template>
 
